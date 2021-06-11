f3e06d6c9c25b03c129ed629aab2d1cab3a38963
Plamedie Bitota <219260532@mycput.ac.za>
2021-06-11 17:54:24 +0200

    Add factory & Test final

--- src/test/java/za/ac/cput/Factory/BookGenreFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/BookGenreFactoryTest.java
@@ -1,16 +1,45 @@
 package za.ac.cput.Factory;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import za.ac.cput.Entity.BookGenre;
 import za.ac.cput.Entity.Genre;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class BookGenreFactoryTest { @Test
-public void  createGenre()
+class BookGenreFactoryTest
 {
-    BookGenre bookGenre=BookGenreFactory.createBookGenre();
-    System.out.println(bookGenre);
-}
+    private BookGenre b1;
+    private BookGenre b2;
+    @Test
+public void  createBookGenre()
+{
+    b1=BookGenreFactory.createBookGenre();
+    b2=BookGenreFactory.createBookGenre();
 
+
+}
+@Test
+void TestEquality()
+{
+   assertEquals(b1,b2);
+}
+@Test
+public void testIdent(){
+    assertSame(b1, b1);
+}
+    @Test
+    @Timeout(value= 250, unit = TimeUnit.MILLISECONDS)
+    void TestTimeout()
+    {
+        createBookGenre();
+    }
+    @Test
+    @Disabled
+    void TestDisabled(){
+        createBookGenre();
+    }
 }
--- src/test/java/za/ac/cput/Factory/GenreFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/GenreFactoryTest.java
@@ -1,16 +1,43 @@
 package za.ac.cput.Factory;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import za.ac.cput.Entity.BookGenre;
 import za.ac.cput.Entity.Genre;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GenreFactoryTest
 {
+    private Genre g1;
+    private Genre g2;
     @Test
     public void  createGenre()
     {
         Genre genre=GenreFactory.createGenre("Romance");
         System.out.println(genre);
+    }
+    @Test
+    void TestEquality()
+    {
+        assertEquals(g1,g2);
+    }
+    @Test
+    public void testIdentity(){
+        assertSame(g1, g2);
+    }
+    @Test
+    @Timeout(value= 250, unit = TimeUnit.MILLISECONDS)
+    void TestTimeout()
+    {
+        createGenre();
+    }
+    @Test
+    @Disabled
+    void TestDisabled(){
+        createGenre();
     }
 }
