158386da151d0b39d1c9797ca3ffa54369e59548
ABurger219014868 <219014868@mycput.ac.za>
2021-06-11 17:19:58 +0200

    Add more test cases to FacoryTests

--- src/test/java/za/ac/cput/Factory/RoleFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/RoleFactoryTest.java
@@ -1,21 +1,54 @@
 package za.ac.cput.Factory;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import za.ac.cput.Entity.Role;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 /*  RoleFactoryTest.java
     Factory Test for the Role entity
     Author: Adriaan Burger(219014868)
-    Date: 10 June 2021
+    Date: 11 June 2021
  */
 
 class RoleFactoryTest {
+    private Role role1, role2;
+
     @Test
     public void createRole(){
-        Role role = RoleFactory.createRole("student",null);
-        System.out.println(role);
-        assertNotNull(role);
+        role1 = RoleFactory.createRole("student",null);
+        role2 = RoleFactory.createRole("librarian",null);
+        System.out.println(role1);
+        System.out.println(role2);
+        assertNotNull(role1);
+        assertNotNull(role2);
     }
+
+    @Test
+    public void testEqual(){
+        assertEquals(role1, role2);
+    }
+
+    @Test
+    public void testIdent(){
+        assertSame(role1, role2);
+    }
+
+    @Test
+    @Timeout(value = 110, unit = TimeUnit.MILLISECONDS)
+    void testTime(){
+        createRole();
+    }
+
+    @Test
+    @Disabled
+    void testSandbox(){
+        //ideas/incomplete tests go here
+        createRole();
+    }
+
 
 }
