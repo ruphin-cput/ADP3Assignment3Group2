56ff9b066f7e2cd06f1889b35baffb74a87a71c1
TaahirBoltman <62078864+TaahirBoltman@users.noreply.github.com>
2021-10-20 11:08:52 +0200

    Updated Book files

--- src/main/java/za/ac/cput/repository/IBookRepository.java
+++ src/main/java/za/ac/cput/repository/IBookRepository.java
@@ -1,5 +1,8 @@
 package za.ac.cput.repository;
-
+/*  IBookRepository.java
+    Book Repository Interface
+    Author: Taahir Boltman(218022972)
+ */
 import za.ac.cput.entity.Book;
 
 import java.util.*;
